dbbebab3d87f9f07ccce2a2c8a15c770386cd506
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-14 13:14:20 +0100

    finito

---
+++ js/script.js
@@ -0,0 +1,15 @@
+for(let i = 0; i < 100; i++){
+    let currNum = i+1;
+    if(currNum % 3 === 0 && currNum % 5 === 0){
+        console.log(`${currNum} FizzBuzz`);
+    }
+    else if(currNum % 3 === 0){
+        console.log(`${currNum} Fizz`);
+    }
+    else if(currNum % 5 === 0){
+        console.log(`${currNum} Buzz`);
+    }
+    else{
+        console.log(currNum);
+    }
+}
